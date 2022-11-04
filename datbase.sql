1eb71182c92ef5a25b08974d3e8fbabd09c6278f
Able <ablekilinda@gmail.com>
2022-11-04 08:39:03 +0300

    signup

--- datbase.sql
+++ datbase.sql
@@ -1,6 +1,6 @@
 create table `user`(
     id int(11) auto_increment primary key,
-    fisrt varchar(256) not null,
+    first varchar(256) not null,
     last varchar (256) not null,
     username varchar(256) not null,
     password varchar (256) not null
--- signup.php
+++ signup.php
@@ -5,4 +5,10 @@ if (isset($_POST['signup-btn'])){
     $uid = $_POST['uid'];
     $pass = $_POST['pwd'];
 
+    $sql = "insert into user(first,last,username,password) values($first,$last,$uid,$pass)";
+    $result = mysqli_query($conn,$sql);
+    if($result = true){
+        echo "success";
+        header("Location: index.php?success");
+    }
 }
