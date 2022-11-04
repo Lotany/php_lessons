3fb1fdd98ccc40a36ec19f87109ddc6205d26e4f
Able <ablekilinda@gmail.com>
2022-11-04 10:39:24 +0300

    add profiles

--- upload.php
+++ upload.php
@@ -1,7 +1,8 @@
 <?php
-
 session_start();
  include_once 'inc/config.php';
+
+ $id = $_SESSION['id'];
 
  if(isset($_POST['upload'])){
     $file = $_FILES['file'];
@@ -16,6 +17,21 @@ session_start();
     $fileActualext = strtolower(end($fileext));
     $allowed = array('jpg','jpeg','png','pdf');
 
-
+  if(in_array($fileActualext,$allowed)){
+    if($fileeeror === 0){
+        if($filesize <1000000){
+            $fileNameNew ="profile".$id.".".$fileActualext;
+            $filedestination = 'uploads/'.$fileNameNew;
+            move_uploaded_file($fileTmpname,$filedestination);
+            header("Location: index.php?uplloadsuccsess");
+        }else {
+            echo "Your file is too big";
+        }
+    }else {
+        echo "There was an error uploading your file";
+    }
+  }else{
+    echo "ou cannot upload files of this type";
+  }
     
  }
