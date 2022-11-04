e3b0e7547cc8a5f7b9b3cc95ddf7f7dec8a478b5
Able <ablekilinda@gmail.com>
2022-11-04 11:26:22 +0300

    lotan

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ include 'inc/config.php';
      while($rowimg=mysqli_fetch_assoc($resultimg)){
         echo "<div>";
         if ($rowimg['status'] == 0){
-            echo "<img src='image/approve1'.$id.'.gif'>";
+            echo "<img src='image/approve".$id.".gif'>";
         }else {
             echo "<img src='image/approve_icon.gif'>";
         }
