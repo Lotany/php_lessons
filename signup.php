524f412a584d0d491051c8044514d52dd90884bd
Able <ablekilinda@gmail.com>
2022-11-04 09:12:44 +0300

    lotan

--- signup.php
+++ signup.php
@@ -10,5 +10,16 @@ if (isset($_POST['signup-btn'])){
     mysqli_query($conn,$sql);
 
     $sql1= "select * from user where username ='$uid' and first='$first'";
+    $result = mysqli_query($conn,$sql1);
+    if (mysqli_num_rows($result) > 0){
+       while($row =mysqli_fetch_assoc($result)){
+        $userid =$row['id'];
+        $sql = "insert into profileimg(userid,status) values('$userid',1)";
+        mysqli_query($conn,$sql);
+        header("Location: index.php");
+       }
+    }else{
+        "error";
+    }
 
 }
