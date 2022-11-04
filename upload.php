edde8f7d05c690d6049aa9845ddc186593c0ac5a
Able <ablekilinda@gmail.com>
2022-11-04 10:25:49 +0300

    lotan

--- upload.php
+++ upload.php
@@ -1,4 +1,21 @@
 <?php
+
+session_start();
  include_once 'inc/config.php';
 
- 
+ if(isset($_POST['upload'])){
+    $file = $_FILES['file'];
+
+    $fileName =$file['name'];
+    $fileTmpname =$file['tmp_name'];
+    $filesize = $file['size'];
+    $fileeeror = $file['error'];
+    $filetype =$file['type'];
+
+    $fileext =explode('.',$fileName);
+    $fileActualext = strtolower(end($fileext));
+    $allowed = array('jpg','jpeg','png','pdf');
+
+
+    
+ }
