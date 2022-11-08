4ba32c554a48fe4ab4336249c20379f9e1b9285d
Able <ablekilinda@gmail.com>
2022-11-08 05:37:27 +0300

    adding a style

--- delete.php
+++ delete.php
@@ -2,3 +2,7 @@
 session_start();
 include_once 'inc/config.php';
 
+$sessionid= $_SESSION['id'];
+$filename = "image/approve".$sessionid."*";
+$fileinfo = glob($filename);
+$fileext = explode(".", $fileinfo[0]);
