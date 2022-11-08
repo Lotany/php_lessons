6f9b9cf109c9bbd376331619d9e5f5b0c218e45c
Able <ablekilinda@gmail.com>
2022-11-08 05:46:55 +0300

    lotan

--- delete.php
+++ delete.php
@@ -8,3 +8,17 @@ $fileinfo = glob($filename);
 $fileext = explode(".", $fileinfo[0]);
 
 $fileactualext = $fileext[1];
+
+$file = "image/approve".$sessionid.".".$fileactualext;
+
+if(!unlink($file)){
+    echo "File was not deleted";
+
+}else{
+    echo "File was deleted";
+}
+
+$sql = "update profileimg set status= 1 where userid = '$sessionid';";
+$result = mysqli_query($conn, $sql);
+
+header("Location: index.php?deletecomplete");
